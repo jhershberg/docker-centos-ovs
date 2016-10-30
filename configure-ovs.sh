4e24c49af19dd69d876f6da46051036d8500ba34
Josh <jhershbe@redhat.com>
2016-10-30 16:41:20 +0200

    Image with two nics...
    One for management and one for "external"
    Only the mgmt nic is bound by ovsdb-server
    A provider mapping is added for the external nic
    
    Signed-off-by: Josh <jhershbe@redhat.com>

--- configure-ovs.sh
+++ configure-ovs.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 ovs_version=$(ovs-vsctl -V | grep ovs-vsctl | awk '{print $4}')
 ovs_db_version=$(ovsdb-tool schema-version /usr/share/openvswitch/vswitch.ovsschema)
+bind_addr=$(ip a | grep -o 172.28.5.1.)
+ext_iface=$(ip a | awk '/172.29.5.1/ {print $5}')
 
 # give ovsdb-server and vswitchd some space...
 sleep 3
@@ -11,6 +13,7 @@ ovs-vsctl --no-wait -- set Open_vSwitch . ovs-version="${ovs_version}"
 ovs-vsctl --no-wait -- set Open_vSwitch . system-type="docker-ovs"
 ovs-vsctl --no-wait -- set Open_vSwitch . system-version="0.1"
 ovs-vsctl --no-wait -- set Open_vSwitch . external-ids:system-id=`cat /proc/sys/kernel/random/uuid`
-ovs-vsctl --no-wait -- set-manager ptcp:6640
+ovs-vsctl --no-wait -- set Open_vSwitch . Other_Config:provider_mappings=physnet:$ext_iface
+ovs-vsctl --no-wait -- set-manager ptcp:6640:$bind_addr
 ovs-appctl -t ovsdb-server ovsdb-server/add-remote db:Open_vSwitch,Open_vSwitch,manager_options
 
