625e0631de68fa1958f0303c2be52f3288a4df68
Josh <jhershbe@redhat.com>
2018-02-21 09:49:04 +0200

    Make the OVS actively connect, set local_ip
    Signed-off-by: Josh <jhershbe@redhat.com>

--- configure-ovs.sh
+++ configure-ovs.sh
@@ -11,6 +11,12 @@ ovs-vsctl --no-wait -- set Open_vSwitch . ovs-version="${ovs_version}"
 ovs-vsctl --no-wait -- set Open_vSwitch . system-type="docker-ovs"
 ovs-vsctl --no-wait -- set Open_vSwitch . system-version="0.1"
 ovs-vsctl --no-wait -- set Open_vSwitch . external-ids:system-id=`cat /proc/sys/kernel/random/uuid`
-ovs-vsctl --no-wait -- set-manager ptcp:6640
+
+LOCAL_IP=$(ip a show eth0 | awk '/inet / {print $2}' | grep -o ^[^/]*)
+CONTROLLER_IP=$(ip route | grep default | cut -d\  -f3)
+ovs-vsctl set Open_vSwitch . other_config:local_ip=$LOCAL_IP
+ovs-vsctl --no-wait -- add-br br-int
+ovs-vsctl --no-wait -- set-controller br-int tcp:${CONTROLLER_IP}:6653
+ovs-vsctl --no-wait -- set-manager tcp:${CONTROLLER_IP}:6640
 ovs-appctl -t ovsdb-server ovsdb-server/add-remote db:Open_vSwitch,Open_vSwitch,manager_options
 
