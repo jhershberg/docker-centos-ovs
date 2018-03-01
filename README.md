4a9dc462a8a883eb64de10d120875f52c8a5c65b
Josh <jhershbe@redhat.com>
2018-03-01 09:20:50 +0200

    Add controller modes
    Signed-off-by: Josh <jhershbe@redhat.com>

--- README.md
+++ README.md
@@ -2,12 +2,20 @@
 This directory contains a Dockerfile and supporting files 
 for creating a docker image that runs OpenVSwitch on CentOS.
 
-## Usage
+## Build
 1. Obtain or compile an RPM of OpenVSwitch. The directions in ovs/INSTALL.Fedora work just fine
 2. Place the resulting RPM in in this directory
 3. Edit the OVSRPM environment variable in the Dockerfile to be the name of your RPM
 4. Run docker build on this directory
-5. To run the docker image say, "sudo docker run -itd --cap-add NET_ADMIN name_of_your_image"
+## Run
+The manager and controller connections are managed via an environment variable, MODE,
+which must be passed in when the container is spun up. This MODE may have one of
+the following four forms:
+ * MODE=none - will result in no manager or controller being set
+ * MODE=ptcp - except passive manager tcp conntections 
+ * MODE=tcp - set the manager and controller IPs to the value of the default route
+ * MODE=tcp:<IP> - set the manager and controller IPs to the IP specified. Generally speaking, you will not need this option
+To run the docker image say, "sudo docker run -itd -e MODE=none --cap-add NET_ADMIN name_of_your_image"
 
 ## Credits
 This work is largely based on that of Dave Tucker: https://github.com/dave-tucker/docker-ovs 
--- configure-ovs.sh
+++ configure-ovs.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 ovs_version=$(ovs-vsctl -V | grep ovs-vsctl | awk '{print $4}')
 ovs_db_version=$(ovsdb-tool schema-version /usr/share/openvswitch/vswitch.ovsschema)
+local_ip=$(ip a show eth0 | awk '/inet / {print $2}' | grep -o ^[^/]*)
+mode=${1:-tcp}
 
 # give ovsdb-server and vswitchd some space...
 sleep 3
@@ -11,12 +13,19 @@ ovs-vsctl --no-wait -- set Open_vSwitch . ovs-version="${ovs_version}"
 ovs-vsctl --no-wait -- set Open_vSwitch . system-type="docker-ovs"
 ovs-vsctl --no-wait -- set Open_vSwitch . system-version="0.1"
 ovs-vsctl --no-wait -- set Open_vSwitch . external-ids:system-id=`cat /proc/sys/kernel/random/uuid`
-
-LOCAL_IP=$(ip a show eth0 | awk '/inet / {print $2}' | grep -o ^[^/]*)
-CONTROLLER_IP=$(ip route | grep default | cut -d\  -f3)
-ovs-vsctl set Open_vSwitch . other_config:local_ip=$LOCAL_IP
+ovs-vsctl set Open_vSwitch . other_config:local_ip=$local_ip
 ovs-vsctl --no-wait -- add-br br-int
-ovs-vsctl --no-wait -- set-controller br-int tcp:${CONTROLLER_IP}:6653
-ovs-vsctl --no-wait -- set-manager tcp:${CONTROLLER_IP}:6640
+if [ $mode == "ptcp" ]
+then
+    ovs-vsctl --no-wait -- set-manager ptcp:6640
+elif [ $mode == "none" ]
+then
+    echo nothing
+else
+    controller_ip=$(echo $mode | grep tcp:)
+    controller_ip=${controller_ip:-tcp:$(ip route | grep default | cut -d\  -f3)}
+    ovs-vsctl --no-wait -- set-controller br-int ${controller_ip}:6653
+    ovs-vsctl --no-wait -- set-manager ${controller_ip}:6640
+fi
 ovs-appctl -t ovsdb-server ovsdb-server/add-remote db:Open_vSwitch,Open_vSwitch,manager_options
 
